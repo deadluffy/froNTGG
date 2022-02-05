0b0f8559ec7d8f5d6039f3360a0ce28a25ce99b9
deadluffy <letstrustmenow@gmail.com>
2022-02-05 19:33:48 +0300

    ok da

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import Header from "./components/Header/Header";
 import React from 'react';
 import { Route, Routes } from "react-router-dom";
-import Main from "./components/pages/Main";
-import Ads from "./components/pages/Ads";
-import Navigation from "./components/nav/Navigation";
+import Main from "./components/Pages/Main";
+import Ads from "./components/Pages/Ads";
+import Navigation from './components/Navigation/Navigation'
 const App = () => {
   return (
     <div className="App">
       <Header />
-      <main className="flex mx-32">
+      <main className="flex w-full mx-32">
         <Navigation />
-        <Routes>
+        <Routes> 
           <Route path='/ads' element={<Ads />} />
           <Route path='/' element={<Main />} />
         </Routes>
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import widget from '../../assets/left_widget_img.jpg'
+import { Link } from 'react-router-dom';
+const Navigation = () => {
+
+    const [json, setJson] = useState({});
+
+    React.useEffect(() => {
+        fetch(`https://demo-api.vsdev.space/api/brom/left_widget`)
+            .then(res => res.json())
+            .then(data => setJson(data))
+    }, []);
+
+
+    return (
+        <div className='flex justify-center bg-sky'>
+            <div className='text-xl h-screen mx-20 '>
+                <ul className='flex my-auto flex-col'>
+                    <li className='text-3xl'><Link to="/">О нас</Link></li>
+                    <li className='text-3xl my-8'><Link to="/ads">Объявления</Link></li>
+                </ul>
+                <div className='my-4 text-2xl w-270px'>Количество объявлений<br /> в базе brom.ru</div>
+                <img src={widget} />
+                <ul>
+                    <li>Автомобили: {json.cars}</li>
+                    <li>Квартиры: {json.apartments}</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navigation;
